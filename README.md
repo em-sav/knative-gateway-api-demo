3546f7df4087fd7ae9be562f5d9e36121e562079
Emile Savard <emile.savard.21@gmail.com>
2025-09-07 23:45:34 -0400

    feat: migrate demo with istio gateway-api

---
+++ README.md
@@ -0,0 +1,7 @@
+# CNCF Presentation - Knative Serving with Gateway API plugin
+
+## Prequisites
+
+```bash
+sudo docker-mac-net-connect
+```
--- k8s/bootstrap.sh
+++ k8s/bootstrap.sh
@@ -3,10 +3,21 @@ REPO_DIR="$(dirname "$(realpath "$0")")/.."
 # Create local Cluster
 kind create cluster --config="$REPO_DIR/k8s/kind/config.yaml" --name=cncf-cluster
 
+# Install gateway API CRDs
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.3.0/standard-install.yaml
+
 # Install and Configure Envoy Gateway
-helm upgrade --install eg oci://docker.io/envoyproxy/gateway-helm --set config.envoyGateway.provider.kubernetes.deploy.type=GatewayNamespace --version v1.5.0 -n envoy-gateway-system --create-namespace
-kubectl wait --timeout=5m -n envoy-gateway-system deployment/envoy-gateway --for=condition=Available
-kubectl apply -f "$REPO_DIR/k8s/envoy-gateway/"
+# helm upgrade --install eg oci://docker.io/envoyproxy/gateway-helm --set config.envoyGateway.provider.kubernetes.deploy.type=GatewayNamespace --version v1.5.0 -n envoy-gateway-system --create-namespace
+# kubectl wait --timeout=5m -n envoy-gateway-system deployment/envoy-gateway --for=condition=Available
+# kubectl apply -f "$REPO_DIR/k8s/envoy-gateway/"
+
+# Install Istio
+helm repo add istio-official https://istio-release.storage.googleapis.com/charts
+helm upgrade --install istio-base istio-official/base --version 1.27.1 -n istio-system --create-namespace
+helm upgrade --install istiod istio-official/istiod --version 1.27.1 -n istio-system
+kubectl wait --timeout=5m -n istio-system deployment/istiod --for=condition=Available
+
+kubectl apply -f "$REPO_DIR/k8s/istio/"
 
 # Install and Configure MetalLB
 helm repo add metallb https://metallb.github.io/metallb
@@ -25,11 +36,14 @@ kubectl wait --timeout=5m -n knative-operator deployment/knative-operator --for=
 
 kubectl apply -f "$REPO_DIR/k8s/knative/01-ns.yaml"
 kubectl apply -f "$REPO_DIR/k8s/knative/02-knative-serving.yaml"
+kubectl label namespace knative-serving istio-injection=enabled # We need to manually label the knative-serving namespace to be in the Istio dataplane
+sleep 15
 kubectl wait --timeout=5m -n knative-serving deployment/controller --for=condition=Available
 
 kubectl apply -f "$REPO_DIR/k8s/knative/03-net-gateway-api.yaml"
 kubectl wait --timeout=5m -n knative-serving deployment/net-gateway-api-controller --for=condition=Available
 
 # Install our Workload
+sleep 15
 kubectl apply -f "$REPO_DIR/k8s/workload/"
 
